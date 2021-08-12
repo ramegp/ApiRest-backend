10eb8ea2e4f8ef5bb81d30415e07e01ece2961ef
Ramiro <ramegp@gmail.com>
2021-08-12 18:59:21 -0300

    modifique los path

--- src/main.js
+++ src/main.js
@@ -4,45 +4,65 @@ var express = require("express");
 var fs = require("fs");
 var app = express();
 var puerto = 8080;
+var path = "./assets";
 var server = app.listen(puerto, function () {
-    console.log("servidor inicializado en el puerto " + server.address());
+    console.log("servidor inicializado en el puerto " + puerto);
 });
-app.get('/', function (req, res) {
-    res.json({ msg: 'text' });
+var crearArchivo = function (nombreArchivo) {
+    var visitas = [
+        {
+            items: 0,
+            item: 0
+        },
+    ];
+    // if (!fs.existsSync(nombreArchivo)) { //esto seria si quiero guardar el contenido
+    //   fs.writeFileSync(
+    //   path + "/" + nombreArchivo,
+    //   JSON.stringify(visitas, null, "\t")
+    // );
+    // }
+    fs.writeFileSync(path + "/" + nombreArchivo, JSON.stringify(visitas, null, "\t"));
+    //esto es para pisarlo cada vez que inicio el server
+};
+crearArchivo("visitas.txt");
+app.get("/", function (req, res) {
+    return res.json({ msg: "Home" });
 });
-app.get('/mundo', function (req, res) {
-    res.send('<h1 style="color:red">Hola mundo</h1>');
-});
-app.get('/productos', function (req, res) {
-    var rand = function (min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    };
-    fs.promises.readFile('./productos.txt').then(function (data) { return data.toString('utf-8'); }).then(function (datos) {
-        var json = JSON.parse(datos);
-        res.json({ item: json[rand(0, json.length - 1)] });
-    });
-});
-app.get('/items', function (req, res) {
-    var visitas = JSON.parse(fs.readFileSync('./assets/visitas.txt', 'utf-8'));
+app.get("/items", function (req, res) {
+    var visitas = JSON.parse(fs.readFileSync(path + "/visitas.txt", "utf-8"));
     visitas[0].items++;
-    fs.writeFileSync('./assets/visitas.txt', JSON.stringify(visitas, null, '\t'));
-    fs.promises.readFile('./assets/productos.txt').then(function (data) { return data.toString('utf-8'); }).then(function (datos) {
-        res.json({ items: JSON.parse(datos), cantidad: JSON.parse(datos).length });
+    fs.writeFileSync(path + "/visitas.txt", JSON.stringify(visitas, null, "\t"));
+    fs.promises
+        .readFile(path + "/productos.txt")
+        .then(function (data) { return data.toString("utf-8"); })
+        .then(function (datos) {
+        res.json({
+            items: JSON.parse(datos),
+            cantidad: JSON.parse(datos).length
+        });
     });
 });
-app.get('/item-random', function (req, res) {
+app.get("/item-random", function (req, res) {
     var random = function (min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     };
-    var visitas = JSON.parse(fs.readFileSync('./assets/visitas.txt', 'utf-8'));
+    var visitas = JSON.parse(fs.readFileSync(path + "/visitas.txt", "utf-8"));
     visitas[0].item++;
-    fs.writeFileSync('./assets/visitas.txt', JSON.stringify(visitas, null, '\t'));
-    fs.promises.readFile('./assets/productos.txt').then(function (data) { return data.toString('utf-8'); }).then(function (datos) {
-        res.json({ item: JSON.parse(datos)[random(0, JSON.parse(datos).length)] });
+    fs.writeFileSync(path + "/visitas.txt", JSON.stringify(visitas, null, "\t"));
+    fs.promises
+        .readFile(path + "/productos.txt")
+        .then(function (data) { return data.toString("utf-8"); })
+        .then(function (datos) {
+        return res.json({
+            item: JSON.parse(datos)[random(0, JSON.parse(datos).length)]
+        });
     });
 });
-app.get('/visitas', function (req, res) {
-    fs.promises.readFile('./assets/visitas.txt').then(function (data) { return data.toString('utf-8'); }).then(function (datos) {
-        res.json({ visitas: JSON.parse(datos)[0] });
+app.get("/visitas", function (req, res) {
+    fs.promises
+        .readFile(path + "/visitas.txt")
+        .then(function (data) { return data.toString("utf-8"); })
+        .then(function (datos) {
+        return res.json({ visitas: JSON.parse(datos)[0] });
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,78 @@
-import * as express from 'express';
-import * as fs from 'fs';
-
+import * as express from "express";
+import * as fs from "fs";
+​
 const app = express();
 const puerto = 8080;
+const path = "./assets";
+
 
 const server = app.listen(puerto, () => {
-    console.log(`servidor inicializado en el puerto ${server.address()}`)
-})
-
-app.get('/', (req: any, res: any) => {
-    res.json({ msg: 'text' })
-})
-
-app.get('/mundo', (req: any, res: any) => {
-    res.send('<h1 style="color:red">Hola mundo</h1>')
-})
-
-
-app.get('/productos', (req: any, res: any) => {
-    let rand = (min: number, max: number): number => {
-        return Math.floor(Math.random() * (max - min) + min)
-    }
-
-    fs.promises.readFile('./productos.txt').then(data => data.toString('utf-8')).then(datos => {
-        const json = JSON.parse(datos)
-
-        res.json({ item: json[rand(0, json.length - 1)] })
-    })
-})
-
-app.get('/items', (req: any, res: any) => {
-
-    const visitas = JSON.parse(fs.readFileSync('./assets/visitas.txt','utf-8'));
-    visitas[0].items++;
-    fs.writeFileSync('./assets/visitas.txt', JSON.stringify(visitas, null, '\t'));
-
-
-    fs.promises.readFile('./assets/productos.txt').then(data => data.toString('utf-8')).then(datos => {
-        res.json({items:JSON.parse(datos),cantidad:JSON.parse(datos).length})
-    })
-})
-
-app.get('/item-random',(req:any,res:any)=>{
-    let random = (min: number, max: number): number => {
-        return Math.floor(Math.random() * (max - min) + min)
-    }
-
-    const visitas = JSON.parse(fs.readFileSync('./assets/visitas.txt','utf-8'));
-    visitas[0].item ++;
-    fs.writeFileSync('./assets/visitas.txt', JSON.stringify(visitas, null, '\t'));
-
-    fs.promises.readFile('./assets/productos.txt').then(data => data.toString('utf-8')).then(datos => {
-        res.json({item:JSON.parse(datos)[random(0,JSON.parse(datos).length)]})
-    })
-
-})
-
-app.get('/visitas',(req:any,res:any)=>{
-    fs.promises.readFile('./assets/visitas.txt').then(data => data.toString('utf-8')).then(datos => {
-        res.json({visitas:JSON.parse(datos)[0]})
-    })
-})
+  console.log(`servidor inicializado en el puerto ${puerto}`);
+});
+const crearArchivo = (nombreArchivo: string) => {
+  const visitas = [
+    {
+      items: 0,
+      item: 0,
+    },
+  ];
+  // if (!fs.existsSync(nombreArchivo)) { //esto seria si quiero guardar el contenido
+  //   fs.writeFileSync(
+  //   path + "/" + nombreArchivo,
+  //   JSON.stringify(visitas, null, "\t")
+  // );
+  // }
+  fs.writeFileSync(
+    path + "/" + nombreArchivo,
+    JSON.stringify(visitas, null, "\t")
+  );
+  //esto es para pisarlo cada vez que inicio el server
+};
+crearArchivo("visitas.txt");
+app.get("/", (req: express.Request, res: express.Response) => {
+  return res.json({ msg: "Home" });
+});
+​
+app.get("/items", (req: express.Request, res: express.Response) => {
+  const visitas = JSON.parse(fs.readFileSync(path + "/visitas.txt", "utf-8"));
+  visitas[0].items++;
+  fs.writeFileSync(path + "/visitas.txt", JSON.stringify(visitas, null, "\t"));
+​
+  fs.promises
+    .readFile(path + "/productos.txt")
+    .then((data) => data.toString("utf-8"))
+    .then((datos) => {
+      res.json({
+        items: JSON.parse(datos),
+        cantidad: JSON.parse(datos).length,
+      });
+    });
+});
+​
+app.get("/item-random", (req: express.Request, res: express.Response) => {
+  let random = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+​
+  const visitas = JSON.parse(fs.readFileSync(path + "/visitas.txt", "utf-8"));
+  visitas[0].item++;
+  fs.writeFileSync(path + "/visitas.txt", JSON.stringify(visitas, null, "\t"));
+​
+  fs.promises
+    .readFile(path + "/productos.txt")
+    .then((data) => data.toString("utf-8"))
+    .then((datos) => {
+      return res.json({
+        item: JSON.parse(datos)[random(0, JSON.parse(datos).length)],
+      });
+    });
+});
+​
+app.get("/visitas", (req: express.Request, res: express.Response) => {
+  fs.promises
+    .readFile(path + "/visitas.txt")
+    .then((data) => data.toString("utf-8"))
+    .then((datos) => {
+      return res.json({ visitas: JSON.parse(datos)[0] });
+    });
+});
