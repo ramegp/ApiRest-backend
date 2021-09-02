7323ca0322954f0c3fef0059aed698ce80f94981
Ramiro <ramegp@gmail.com>
2021-09-02 20:17:47 -0300

    modifico nombre carpeta

---
+++ 009-Desafio/src/main.ts
@@ -0,0 +1,25 @@
+import express = require("express");
+import * as fs from "fs";
+
+
+
+const app = express();
+const puerto = 8080;
+
+const path = __dirname + "/../assets";
+const api = require('./rutas/productos.route');
+
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req:any,res:any)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
+
+app.use('/api',api);
+app.use(express.static('public'));
+
+const server = app.listen(puerto, () => {
+    console.log(`servidor inicializado en el puerto ${puerto}`);
+});
